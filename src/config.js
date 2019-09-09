50e04e4c94403c45deb73494808140408c8523e9
Leila Anderson <leilacanderson@gmail.com>
2019-09-08 20:58:02 -0500

    fixed notes bug

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     API_KEY: process.env.REACT_APP_API_KEY,
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://guarded-taiga-77278.herokuapp.com/api/',
     TOKEN_KEY: 'bibliofile-client-auth-token'
   }
